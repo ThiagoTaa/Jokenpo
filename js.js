d79ad9b202115c206bc644aeab961f29ac621183
Thiago Alves Alencar <thiagoalencar.taa@gmail.com>
2020-11-18 17:37:51 -0300

    html e funções basicas

---
+++ js.js
@@ -0,0 +1,8 @@
+var jogadorNome;
+document.getElementById('jogador-escolha1').onclick = function(){};
+document.getElementById('jogador-escolha2').onclick = function(){};
+document.getElementById('jogador-escolha3').onclick = function(){};
+
+JogadorNome = prompt('Qual é o seu nome?');
+
+document.getElementById('mensagem').innerHTML = JogadorNome;
